4da43d42a837dc321fa36bb8b884ffdc00978ae8
dev-hive <angrishkartikay2013@gmail.com>
2026-09-26 16:01:07 +0530

    Use a safer default model learning rate

--- include/model.hpp
+++ include/model.hpp
@@ -25,7 +25,7 @@ public:
     float train(
         const std::string& text,
         std::size_t epochs = 1,
-        float learning_rate = 0.003f,
+        float learning_rate = 0.001f,
         const std::function<void(
             std::size_t,
             float)>& progress = {});
